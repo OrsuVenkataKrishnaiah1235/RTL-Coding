a9954fb6e74c30bf6a3c386c2e2451a09ceddf9b
OrsuVenkataKrishnaiah1235 <orsuvenkatakrishnaiah1235@gmail.com>
2023-02-24 15:46:27 +0530

    Verilog

---
+++ HDL_BITS/5.More_Verilog_feautures/6.Add100i/add100i.v
@@ -0,0 +1,3 @@
+//In Verilog, genvar is a special keyword used to declare variables that are used for generating loops and instantiating multiple instances of a module.
+
+//Each instantiation of the one_bit_FA module is given a label using the :Full_adder_block syntax. This label is used to identify the instance of the module in the design hierarchy, and can be useful for debugging and other purposes.
